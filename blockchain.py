041f70c6b41cbe18c0093ec5327c904eb259a793
Гарнец Игорь Дмитриевич <garnets@suiljex.space>
2020-03-15 12:36:32 +0700

    DEL Лишний код

--- blockchain.py
+++ blockchain.py
@@ -7,50 +7,17 @@ class Blockchain:
     self._chain = list()
     self._crypto = BlockchainCrypto()
     self._validator = BlockchainValidator()
-    # self._hight = 0
-    # self._used_transactions = set()
-    # self._txs_list = dict()
     self._genesis_block()
 
-  # @property
   def export_chain(self):
     return self._chain
 
   def import_chain(self, chain):
-    # hight = 0
-    # used_transactions = set()
-    # txs_list = dict()
-    # previous_block = None
-
-    # hight = 0
-    # used_transactions = set()
-    # txs_list = dict()
-    # previous_block = None
-
-    # if len(chain) <= 1:
-    #   return True
-
-    # previous_block = chain[0]
-    # for block in chain[1:]:
-    #   if self._validator.valid_block(block, previous_block, hight, txs_list, used_transactions) == False:
-    #     return False
-    #   previous_block = block
-    #   hight += 1
-
     if self._validator.valid_chain(chain) == True:
       self._chain = chain
       return True
 
     return False
-    # self._hight = hight
-    # self._used_transactions = used_transactions
-    # self._txs_list = txs_list
-
-    # for transction in self._pending_transactions:
-    #   if self._valid_transaction(transction, self._txs_list, self._used_transactions) == False:
-    #     del transction
-
-    # return True
         
   def _genesis_block(self):
     self._chain = list()
