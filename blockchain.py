70c568da602fd8b012902ccb9dea56117838990b
Гарнец Игорь Дмитриевич <garnets@suiljex.space>
2020-03-17 10:21:44 +0700

    FIX Оформление

--- blockchain.py
+++ blockchain.py
@@ -1,62 +1,62 @@
 import time
+
 from bccrypto import BlockchainCrypto
 from bcvalidator import BlockchainValidator
 
+
 class Blockchain:
-  def __init__(self):
-    self._chain = list()
-    self._crypto = BlockchainCrypto()
-    self._validator = BlockchainValidator()
-    self._genesis_block()
+    def __init__(self):
+        self._blockchain = list()
+        self._crypto = BlockchainCrypto()
+        self._validator = BlockchainValidator()
+        self._genesis_block()
 
-  def export_chain(self):
-    return self._chain
+    def export_chain(self):
+        return self._blockchain
 
-  def import_chain(self, chain):
-    if self._validator.valid_chain(chain) == True:
-      self._chain = chain
-      return True
+    def import_chain(self, chain):
+        if self._validator.valid_chain(chain) is True:
+            self._chain = chain
+            return True
 
-    return False
-        
-  def _genesis_block(self):
-    self._chain = list()
+        return False
 
-    data = {
-      'transactions' : list()
-    }
+    def _genesis_block(self):
+        self._blockchain = list()
 
-    header = {
-      'version' : 1,
-      'timestamp' : time.time(),
-      'previous_block' : "0" * 32,
-      'nonce' : 0,
-      'difficulty' : 0,
-      'data_hash' : self._crypto.hash(data)
-    }
+        data = {
+            'transactions': list()
+        }
 
-    block = {
-      'header' : header,
-      'data' : data
-    }
+        header = {
+            'version': 1,
+            'timestamp': time.time(),
+            'previous_block': "0" * 32,
+            'nonce': 0,
+            'difficulty': 0,
+            'data_hash': self._crypto.hash(data)
+        }
 
-    self._chain.append(block)
-    return block
-    
-  def new_block(self, block):
-    if self._validator.valid_block(block, self._chain) == True:
-      # self.pending_transactions = []
-      self._chain.append(block)
-      return True
-    return False
+        block = {
+            'header': header,
+            'data': data
+        }
 
-  # def resolve_conflicts(self):
-  #   pass
+        self._blockchain.append(block)
+        return block
 
-  @property
-  def last_block(self):
-    return self._chain[-1]
+    def new_block(self, block):
+        if self._validator.valid_block(block, self._chain) is True:
+            # self.pending_transactions = []
+            self._blockchain.append(block)
+            return True
+        return False
+
+    @property
+    def last_block(self):
+        return self._blockchain[-1]
+
 
 if __name__ == '__main__':
-  print("This module is a dependecy")
-  exit(0)
+    print("This module is a dependecy")
+    exit(0)
